964bf218649fa184bb2d17c6e5c744a0bd6a5128
James Pier <jpier2012@gmail.com>
2019-04-12 17:10:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,46 +1,46 @@
 require 'pry'
 
 
-def items
-	[
-		{"AVOCADO" => {:price => 3.00, :clearance => true}},
-		{"KALE" => {:price => 3.00, :clearance => false}},
-		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
-		{"ALMONDS" => {:price => 9.00, :clearance => false}},
-		{"TEMPEH" => {:price => 3.00, :clearance => true}},
-		{"CHEESE" => {:price => 6.50, :clearance => false}},
-		{"BEER" => {:price => 13.00, :clearance => false}},
-		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-		{"BEETS" => {:price => 2.50, :clearance => false}}
-	]
-end
-
-def coupons
-	[
-		{:item => "AVOCADO", :num => 2, :cost => 5.00},
-		{:item => "BEER", :num => 2, :cost => 20.00},
-		{:item => "CHEESE", :num => 3, :cost => 15.00}
-	]
-end
-
-def generate_cart
-	[].tap do |cart|
-		rand(20).times do
-			cart.push(items.sample)
-		end
-	end
-end
-
-def generate_coupons
-	[].tap do |c|
-		rand(2).times do
-			c.push(coupons.sample)
-		end
-	end
-end
-
-cart_list = generate_cart
-coupons_list = generate_coupons
+# def items
+# 	[
+# 		{"AVOCADO" => {:price => 3.00, :clearance => true}},
+# 		{"KALE" => {:price => 3.00, :clearance => false}},
+# 		{"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+# 		{"ALMONDS" => {:price => 9.00, :clearance => false}},
+# 		{"TEMPEH" => {:price => 3.00, :clearance => true}},
+# 		{"CHEESE" => {:price => 6.50, :clearance => false}},
+# 		{"BEER" => {:price => 13.00, :clearance => false}},
+# 		{"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+# 		{"BEETS" => {:price => 2.50, :clearance => false}}
+# 	]
+# end
+#
+# def coupons
+# 	[
+# 		{:item => "AVOCADO", :num => 2, :cost => 5.00},
+# 		{:item => "BEER", :num => 2, :cost => 20.00},
+# 		{:item => "CHEESE", :num => 3, :cost => 15.00}
+# 	]
+# end
+#
+# def generate_cart
+# 	[].tap do |cart|
+# 		rand(20).times do
+# 			cart.push(items.sample)
+# 		end
+# 	end
+# end
+#
+# def generate_coupons
+# 	[].tap do |c|
+# 		rand(2).times do
+# 			c.push(coupons.sample)
+# 		end
+# 	end
+# end
+#
+# cart_list = generate_cart
+# coupons_list = generate_coupons
 
 def consolidate_cart(cart)
   #binding.pry
